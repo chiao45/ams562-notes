0a86141a819b1d84772bbafba74aac845ff6b805
Qiao Chen <benechiao@gmail.com>
2018-08-21 16:52:58 -0400

    added ipython in sphinx dep

--- conf.py
+++ conf.py
@@ -44,6 +44,8 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.githubpages',
     'sphinx.ext.imgmath',
+    'IPython.sphinxext.ipython_console_highlighting',
+    'IPython.sphinxext.ipython_directive'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
