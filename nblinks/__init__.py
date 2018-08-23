9bfdf6b75c3a406581f07c3d11ea2883dd890e6d
Qiao Chen <benechiao@gmail.com>
2018-08-22 23:22:24 -0400

    added raw source file support

--- nblinks/__init__.py
+++ nblinks/__init__.py
@@ -1,18 +1,18 @@
 """
 Helper module for getting all notebooks in lectures and put them into ext links
-with NBviewer
+with NBviewer, in addition, the raw cpp sources will be included.
 """
 
 from __future__ import print_function
 import os
 
 __repo_abs_path__ = os.path.abspath(
-    os.path.dirname(os.path.abspath(__file__))+os.sep+'..')
+    os.path.dirname(os.path.abspath(__file__)) + os.sep + '..')
 
 # get branch
 
-__branch__ = open(__repo_abs_path__+os.sep+'.git' +
-                  os.sep+'HEAD', 'r').read()[:-2].split('/')[-1]
+__branch__ = open(__repo_abs_path__ + os.sep + '.git' +
+                  os.sep + 'HEAD', 'r').read()[:-2].split('/')[-1]
 
 print('ams562-notes branch: %s' % __branch__)
 
@@ -22,19 +22,29 @@ __lec_path__ = __repo_abs_path__ + os.sep + 'lectures'
 #  lectures/<num>/*.ipynb
 
 __lecs__ = os.listdir(__lec_path__)
-__lecs__ = [lec for lec in __lecs__ if os.path.isdir(__lec_path__+os.sep+lec)]
+__lecs__ = [lec for lec in __lecs__ if os.path.isdir(
+    __lec_path__ + os.sep + lec)]
 
-__nbview_pattern__ = 'https://nbviewer.jupyter.org/github/chiao45/ams562-notes/blob/{}/lectures/%s/'.format(__branch__)
+__nbview_pattern__ = 'https://nbviewer.jupyter.org/github/chiao45/ams562-notes/blob/{}/lectures/%s/'.format(
+    __branch__)
+__cpp_source__ = 'https://raw.githubusercontent.com/chiao45/ams562-notes/{}/lectures/%s/'.format(
+    __branch__)
 
 _extlinks = {}
 __alias_pattern__ = 'nblec_%s'
+__cpp_src_pattern__ = 'cpplec_%s'
 __prefix__ = 'notebook '
+__cpp_prefix__ = 'program '
 
 for lec in __lecs__:
     pat = __alias_pattern__ % lec
     url = __nbview_pattern__ % lec
     url += '%s.ipynb'
+    pat_cpp = __cpp_src_pattern__ % lec
+    url_cpp = __cpp_source__ % lec
+    url_cpp += '%s.cpp'
     _extlinks[pat] = (url, __prefix__)
+    _extlinks[pat_cpp] = (url_cpp, __cpp_prefix__)
 
 # NOTE by default, when we write :nblec_?:`sample`, it will show as
 # notebook sample
