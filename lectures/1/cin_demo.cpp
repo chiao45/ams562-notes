0617f58b988a0af18c0f51333ce399ab9cac7911
Qiao Chen <benechiao@gmail.com>
2018-08-22 23:22:37 -0400

    added main program

---
+++ lectures/1/cin_demo.cpp
@@ -0,0 +1,20 @@
+// AMS 562, Intro to Scientific Computing with C++
+// demo program
+// author: Qiao Chen
+//
+// how to compile:
+//    g++ cin_demo.cpp -o cin_demo
+// how to run:
+//    ./cin_demo
+
+#include <iostream>
+
+int main() {
+  // best practice, always indicate the user what to enter
+  std::cout << "Please enter your first and last names:\n";
+  std::string fname, lname;
+  // the program will hang here til recieve the user input
+  std::cin >> fname >> lname;
+  std::cout << "Hello! " << fname << ' ' << lname << std::endl;
+  return 0;
+}
