707874ed90aa5c2d0f267a1030268a0d062bb87d
Adrian Hurtado <ahurta92@gmail.com>
2021-09-02 19:16:54 -0400

    lecuture1 code

--- lecture_code/lecture0/ex1.cpp
+++ lecture_code/lecture0/ex1.cpp
@@ -1,3 +1,3 @@
 
 
-int main() { return 0; }
+int main() { return 0; }  // minimal C++ program
---
+++ lecture_code/tour/ch1_Basics/printmul2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+double mul2(double x) { return x * 2; }
+void printxmul2(double x) {
+  std::cout << "2 * " << x << " is " << mul2(x) << "\n";
+}
+
+int main() {
+
+  printxmul2(2.234);
+
+  return 0;
+}
