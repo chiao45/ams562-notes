7daf18995e745c8e7581e2e793e95f261328a21c
Adrian Hurtado <ahurta92@gmail.com>
2021-09-02 21:22:08 -0400

    move files around to lecture1

--- lecture_code/lecture1/literal.cpp
+++ lecture_code/lecture1/literal.cpp
@@ -1,37 +1,21 @@
 #include <iostream>
 #include <string>
 
-void print_message(int a) {
-  std::cout << "int" << ::std::endl;
-  std::cout << a << ::std::endl;
-}
-void print_message(long int a) {
-  std::cout << "long int" << ::std::endl;
-  std::cout << a << ::std::endl;
-}
+void print_message(int a) { std::cout << "int " << a << ::std::endl; }
+void print_message(long int a) { std::cout << "long int " << a << ::std::endl; }
 void print_message(unsigned long int a) {
-  std::cout << "unsigned long  int" << ::std::endl;
-  std::cout << a << ::std::endl;
+  std::cout << "unsigned long  int " << a << ::std::endl;
 }
 void print_message(unsigned int a) {
-  std::cout << "unsigned  int" << ::std::endl;
-  std::cout << a << ::std::endl;
+  std::cout << "unsigned  int " << a << ::std::endl;
 }
-void print_message(float a) {
-  std::cout << "float" << ::std::endl;
-  std::cout << a << ::std::endl;
-}
-void print_message(double a) {
-  std::cout << "double" << ::std::endl;
-  std::cout << a << ::std::endl;
-}
+void print_message(float a) { std::cout << "float " << a << ::std::endl; }
+void print_message(double a) { std::cout << "double " << a << ::std::endl; }
 void print_message(std::string a) {
-  std::cout << "string " << ::std::endl;
-  std::cout << a << ::std::endl;
+  std::cout << "string " << a << ::std::endl;
 }
 
 int main() {
-
   print_message(-32);
   print_message(-32u);
   print_message(32ul);
--- lecture_code/tour/ch1_Basics/fundamental_types.cpp
+++ lecture_code/tour/ch1_Basics/fundamental_types.cpp
@@ -10,6 +10,9 @@ int main(void) {
   double *ptr;  // pointer to double (address to variable)
   unsigned ui;  // non-negative integer, for example 0,1,999
 
+  unsigned short fu = 0b1111111111111111;
+  short fi = 0b0111111111111111;
+
   std::cout << "size of bool = " << sizeof(b) << " byte" << std::endl;
   std::cout << "size of char = " << sizeof(c) << " byte" << std::endl;
   std::cout << "size of double = " << sizeof(d) << " byte" << std::endl;
@@ -17,4 +20,7 @@ int main(void) {
   std::cout << "size of long = " << sizeof(l) << " byte" << std::endl;
   std::cout << "size of pointer = " << sizeof(ptr) << " byte" << std::endl;
   std::cout << "size of ui = " << sizeof(ui) << " byte" << std::endl;
+
+  std::cout << "max int = " << fi << std::endl;
+  std::cout << "max unsigned = " << fu << std::endl;
 }
