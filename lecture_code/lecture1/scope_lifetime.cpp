d5789b97c5065030a20cc9deed2e9fcc0e660cd7
Adrian Hurtado <ahurta92@gmail.com>
2021-09-03 12:27:21 -0400

    lecture1

--- lecture_code/lecture1/scope_lifetime.cpp
+++ lecture_code/lecture1/scope_lifetime.cpp
@@ -5,25 +5,16 @@
 
 // scope and life time
 int hello = 2;  // global namespace
-
 double square(double a) {
   std::cout << "global hello in square function " << hello << std::endl;
   return a * a;
 }
-
 int main() {
   double local_main = 32;
-
-  {
-    std::cout << local_main << std::endl;
-    double local_scope_d = 23;
-    std::cout << local_scope_d << std::endl;
-  }  // local _scope_d gets destroyed here
+  { double local_scope_d = 23; }  // local _scope_d gets destroyed here
   // std::cout << local_scope_d << std::endl;
-
   std::cout << "global hello in main function" << hello
             << std::endl;  // print global scope hello
   double s3 = square(3);
-
   return 0;
 }
